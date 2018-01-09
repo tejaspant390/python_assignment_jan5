8a414d8c0c4450d8853bbdaf5fed608bdf40fa35
Tejas Pant <tejaspant55@gmail.com>
2018-01-09 12:33:02 +0545

    Question 6 Committed

--- question_6.py
+++ question_6.py
@@ -1,14 +1,4 @@
-my_list=list(range(0,100))
-with open ("e:\\file2.txt","w") as fp:
- for each in my_list:
-     if each<2:
-         fp.write("{}\n".format(each))
+with open ("e:\\file2.txt","r")as fp:
+    data= fp.read()
 
-     else:
-         for i in range(2,each):
-             if each%i==0:
-                 break;
-         else:
-            fp.write("{}\n".format(each))
-
- fp.close()
+    print(data)
